793cd84a0579cb577ab2afefa9a8e1c7077f2048
Spaun1 <andreispaun7@gmail.com>
2024-07-14 05:45:33 +0300

    refactor: rewrite all styles to tailwind css

--- components/game/game-cell.jsx
+++ components/game/game-cell.jsx
@@ -1,11 +1,15 @@
-import { GameSymbol } from './game-symbol';
-import styles from './game.module.css';
+import { clsx } from 'clsx';
 
-export function GameCell({ isWinner, onClick, symbol }) {
+import { GameSymbol } from './game-symbol';
+
+export function GameCell({ isWinner, toggleCell, symbol }) {
   return (
     <button
-      className={`${styles['cell']} ${isWinner ? styles['cell--win'] : ''}`}
-      onClick={onClick}
+      className={clsx(
+        'border border-gray-400 -ml-px -mt-px flex items-center justify-center',
+        isWinner && 'bg-red-400/10'
+      )}
+      onClick={toggleCell}
     >
       {symbol ? <GameSymbol symbol={symbol} /> : null}
     </button>
--- components/game/game.js
+++ components/game/game.js
@@ -1,6 +1,6 @@
 import { GameCell } from './game-cell';
 import { GameInfo } from './game-info';
-import styles from './game.module.css';
+import { ResetButton } from './reset-button';
 import { useGameState } from './use-game-state';
 
 export function Game() {
@@ -15,25 +15,23 @@ export function Game() {
   } = useGameState();
 
   return (
-    <div className={styles['game']}>
+    <div className="flex flex-col items-center w-40 mx-auto my-24 border border-black p-5">
       <GameInfo
         isDraw={isDraw}
         winnerSymbol={winnerSymbol}
         currentStep={currentStep}
       />
-      <div className={styles['game-field']}>
+      <div className="grid pt-px pl-px grid-cols-[repeat(3,_30px)] grid-rows-[repeat(3,_30px)]">
         {cells.map((symbol, index) => (
           <GameCell
             key={index}
             symbol={symbol}
             isWinner={getWinnerCell(index)}
-            onClick={() => toggleCell(index)}
+            toggleCell={() => toggleCell(index)}
           />
         ))}
       </div>
-      <button className={styles['reset']} onClick={resetGame}>
-        Сбросить
-      </button>
+      <ResetButton resetGame={resetGame} />
     </div>
   );
 }
