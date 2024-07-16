3716f500ae2e8f802704ea94a824af85381918bb
Spaun1 <andreispaun7@gmail.com>
2024-07-16 15:19:13 +0300

    add profile components

---
+++ components/Profile/Profile.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+
+import avatarSrc from './assets/avatar.png';
+
+export const Profile = ({ className }) => {
+  return (
+    <div
+      className={clsx(
+        'flex items-center gap-2 text-start text-teal-600',
+        className
+      )}
+    >
+      <Image src={avatarSrc} width={48} height={48} alt="avatar" unoptimized />
+      <div>
+        <div className=" text-lg leading-tight">Paromovevg</div>
+        <div className="text-slate-400 text-xs leading-tight">
+          Рейтинг: 1230
+        </div>
+      </div>
+    </div>
+  );
+};
