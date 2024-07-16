099e9109f430f2e4ce4b887be725105461cfec51
Spaun1 <andreispaun7@gmail.com>
2024-07-16 15:46:34 +0300

    add gameTitle component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,13 @@
 import { Header } from '../components/Header';
+import { GameTitle } from '../components/Game';
 
 export default function HomePage() {
-  return <Header />;
+  return (
+    <div className="bg-slate-50 min-h-screen">
+      <Header />
+      <main className="pt-6 mx-auto max-w-[616px]">
+        <GameTitle />
+      </main>
+    </div>
+  );
 }
